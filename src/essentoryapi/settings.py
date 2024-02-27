af9c3e66d0d2b56f2fc211a5c4bedc2c18577de4
Sung Ho Hwang <sungho.sde@gmail.com>
2024-02-27 03:43:42 -0500

    Move SECRET_KEY to .env

--- src/essentoryapi/settings.py
+++ src/essentoryapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-35&1f=s!gvezp5@r&2_8lrml9!vkrwm0c*t&h^hfzvz7@p4epu"
+SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
