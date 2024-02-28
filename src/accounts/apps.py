2e1e1f9bf9ef06eabd984789223fafba0a6303d8
Sung Ho Hwang <sungho.sde@gmail.com>
2024-02-28 00:28:36 -0500

    Get firebase_admin_sdk_json from .env

--- src/accounts/apps.py
+++ src/accounts/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
 import firebase_admin
 from firebase_admin import credentials
+import os
+import base64
+import json
+
+# Get the encoded string from environment variable
+encoded_json = os.getenv("FIREBASE_ADMIN_SDK_JSON_BASE64")
+
+# Decode the Base64 string
+decoded_json = base64.b64decode(encoded_json)
+
+# Convert to a Python dictionary
+firebase_admin_sdk_json = json.loads(decoded_json)
 
 
 class AccountsConfig(AppConfig):
@@ -8,5 +20,5 @@ class AccountsConfig(AppConfig):
     name = "accounts"
 
     def ready(self):
-        cred = credentials.Certificate("../config/firebase_admin_sdk.json")
+        cred = credentials.Certificate(firebase_admin_sdk_json)
         firebase_admin.initialize_app(cred)
