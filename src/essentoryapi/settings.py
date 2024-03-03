e2b38b0b970d12c908bdc03039680085485875ef
Sung Ho Hwang <sungho.sde@gmail.com>
2024-03-03 18:21:01 -0500

    Set Global throttle rates

--- src/essentoryapi/settings.py
+++ src/essentoryapi/settings.py
@@ -148,6 +148,11 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/hour", "user": "100/hour"},
 }
 
 AUTH_USER_MODEL = "accounts.User"
